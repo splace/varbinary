35310b29d5f041980da3d87092547ada88f8f447
simon <psiplace@netscape.net>
2017-09-30 16:52:35 +0100

    comment

--- example_test.go
+++ example_test.go
@@ -9,9 +9,12 @@ func ExampleUint64_String() {
 	fmt.Println(varbinary.Uint64(0))
 	fmt.Println(varbinary.Uint64(1))
 	fmt.Println(varbinary.Uint64(257))
+	fmt.Println(varbinary.Uint64(65793))
 	// Output:
 	//
 	// 00
 	// 00 00
+	// 00 00 00
 }
+
 
